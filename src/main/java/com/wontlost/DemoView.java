6eaf9f7cc9e12981b8402e58fd8797cad5c8eed2
Ryan Pang <ryan.pang@live.com>
2021-04-15 07:47:13 +1200

    Update

--- src/main/java/com/wontlost/DemoView.java
+++ src/main/java/com/wontlost/DemoView.java
@@ -1,15 +1,21 @@
 package com.wontlost;
 
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.*;
+import com.vaadin.flow.server.PWA;
 import com.wontlost.alert.SweetAlert2View;
 import com.wontlost.ckeditor.views.CKEditorView;
 import com.wontlost.datebook.DatebookView;
 import com.wontlost.dicebear.DicebearView;
 import com.wontlost.zxing.ZXingView;
 
-import static com.wontlost.ckeditor.utils.Constant.PAGE_ROOT;
+import static com.wontlost.ckeditor.utils.Constant.*;
 
 /**
  * @author Ryan Pang
@@ -17,12 +23,13 @@ import static com.wontlost.ckeditor.utils.Constant.PAGE_ROOT;
  */
 @Route("demo")
 @RouteAlias(PAGE_ROOT)
-public class DemoView extends Div implements HasUrlParameter<String> {
+@PWA(name = "Demo", shortName = "Demo", display = "fullscreen")
+public class DemoView extends VerticalLayout implements HasUrlParameter<String> {
 
     @Override
     public void setParameter(BeforeEvent event, @OptionalParameter String parameter) {
         if (parameter == null) {
-            add(new Label("No demo specified!"));
+            createDemoPage();
         } else if (parameter.equals("ckeditor")) {
             add(new CKEditorView());
         } else if (parameter.equals("zxing")) {
@@ -34,6 +41,41 @@ public class DemoView extends Div implements HasUrlParameter<String> {
         } else if (parameter.equals("alert")) {
             add(new SweetAlert2View());
         }
+        setAlignItems(Alignment.CENTER);
+    }
+
+    public void createDemoPage() {
+        FormLayout layout = new FormLayout();
+        Image ckeditor = new Image();
+        Image zxing = new Image();
+        Image dicebear = new Image();
+        Image alert = new Image();
+        Image datebook = new Image();
+
+        ckeditor.setHeight(200, Unit.PIXELS);
+        ckeditor.setWidth(200, Unit.PIXELS);
+        ckeditor.setSrc("icons/ckeditor.png");
+        zxing.setHeight(200, Unit.PIXELS);
+        zxing.setWidth(200, Unit.PIXELS);
+        zxing.setSrc("icons/zxing.png");
+        dicebear.setHeight(200, Unit.PIXELS);
+        dicebear.setWidth(200, Unit.PIXELS);
+        dicebear.setSrc("icons/dicebear.png");
+        alert.setHeight(200, Unit.PIXELS);
+        alert.setWidth(200, Unit.PIXELS);
+        alert.setSrc("icons/sweetAlert.png");
+        datebook.setHeight(200, Unit.PIXELS);
+        datebook.setWidth(200, Unit.PIXELS);
+        datebook.setSrc("icons/datebook.png");
+
+        ckeditor.addClickListener(e-> this.getUI().ifPresent(ui -> ui.navigate(PAGE_DEMO_CKEDITOR)));
+        zxing.addClickListener(e-> this.getUI().ifPresent(ui -> ui.navigate(PAGE_DEMO_ZXING)));
+        dicebear.addClickListener(e-> this.getUI().ifPresent(ui -> ui.navigate(PAGE_DEMO_DICEBEAR)));
+        alert.addClickListener(e-> this.getUI().ifPresent(ui -> ui.navigate(PAGE_DEMO_ALERT)));
+        datebook.addClickListener(e-> this.getUI().ifPresent(ui -> ui.navigate(PAGE_DEMO_DATEBOOK)));
+
+        layout.add(ckeditor, zxing, dicebear, alert, datebook);
+        add(layout);
     }
 
 }
--- src/main/java/com/wontlost/alert/SweetAlert2View.java
+++ src/main/java/com/wontlost/alert/SweetAlert2View.java
@@ -1,16 +1,27 @@
 package com.wontlost.alert;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 import com.wontlost.sweetalert2.Config;
 import com.wontlost.sweetalert2.SweetAlert2Vaadin;
+
+import static com.wontlost.ckeditor.utils.Constant.PAGE_DEMO_ALERT;
+import static com.wontlost.ckeditor.utils.Constant.PAGE_DEMO_ZXING;
 
 /**
  * @author Ryan Pang
  * @date 4/15/2021
  */
+@JsModule("./styles/shared-styles.js")
+@Viewport("width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes, viewport-fit=cover")
+@Route(value = PAGE_DEMO_ALERT)
+@PageTitle("SweetAlert2")
 public class SweetAlert2View extends VerticalLayout {
 
     public SweetAlert2View(){
