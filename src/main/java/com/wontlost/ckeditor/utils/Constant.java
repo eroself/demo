b0b659c51d2ca762b26e4bb236e3a7963f7c8508
Ryan Pang <ryan.pang@live.com>
2021-04-14 23:59:51 +1200

    Update

--- src/main/java/com/wontlost/ckeditor/utils/Constant.java
+++ src/main/java/com/wontlost/ckeditor/utils/Constant.java
@@ -10,17 +10,17 @@ public class Constant {
     public static final String TITLE_DONATE = "Donate";
 
     public static final String PAGE_ROOT = "";
-    public static final String PAGE_DEMO_CKEDITOR = "demo/ckeditor";
-    public static final String PAGE_CLASSIC_EDITOR = "demo/ckeditor/classic";
-    public static final String PAGE_DIALOG_EDITOR = "demo/ckeditor/dialog";
-    public static final String PAGE_BALLOON_EDITOR = "demo/ckeditor/balloon";
-    public static final String PAGE_INLINE_EDITOR = "demo/ckeditor/inline";
-    public static final String PAGE_DECOUPLED_EDITOR  = "demo/ckeditor/document";
-    public static final String PAGE_DONATE = "demo/ckeditor/donate";
+    public static final String PAGE_DEMO_CKEDITOR = "ckeditor";
+    public static final String PAGE_CLASSIC_EDITOR = "ckeditor/classic";
+    public static final String PAGE_DIALOG_EDITOR = "ckeditor/dialog";
+    public static final String PAGE_BALLOON_EDITOR = "ckeditor/balloon";
+    public static final String PAGE_INLINE_EDITOR = "ckeditor/inline";
+    public static final String PAGE_DECOUPLED_EDITOR  = "ckeditor/document";
+    public static final String PAGE_DONATE = "ckeditor/donate";
 
-    public static final String PAGE_DEMO_ZXING = "demo/zxing";
+    public static final String PAGE_DEMO_ZXING = "zxing";
 
-    public static final String PAGE_DEMO_DICEBEAR = "demo/dicebear";
+    public static final String PAGE_DEMO_DICEBEAR = "dicebear";
 
     public static final String VIEWPORT = "width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes, viewport-fit=cover";
 
